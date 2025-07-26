b4e48642f095d0185406037cdb5f60dbc6d26b28
alhwyn <alhwyndwyne0411@gmail.com>
2025-07-25 22:35:20 -0700

    created it

--- util/scraper.ts
+++ util/scraper.ts
@@ -35,7 +35,7 @@ export async function scrapeTextFromUrl(
     excludeTags = "script,style,nav,header,footer,aside",
     includeTags = "",
     maxRedirects = 10,
-    userAgent = "Mozilla/5.0 (compatible; WebScraper/1.0)"
+    userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
   } = options
 
   // Validate URL
